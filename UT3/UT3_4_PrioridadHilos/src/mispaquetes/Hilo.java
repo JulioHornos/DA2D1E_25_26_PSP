dd301e58be74a117da4378ecde851b64c71df9f9
Julio <jhornosb@gmail.com>
2025-11-05 16:41:28 +0100

    Semáforos y prioridad hilos

---
+++ UT3/UT3_4_PrioridadHilos/src/mispaquetes/Hilo.java
@@ -0,0 +1,42 @@
+package mispaquetes;
+
+public class Hilo extends Thread {
+
+    /**************************************************************************
+     * constructor por defecto
+     */
+    public Hilo() {
+        //hereda la prioridad del hilo padre
+    }
+
+    /**************************************************************************
+     * constructor personalizado
+     */
+    public Hilo(int prioridad) {
+
+        //establece la prioridad indicada
+        this.setPriority(prioridad);
+    }
+
+    /**************************************************************************
+     * ejecuta una tarea pesada
+     */
+    @Override
+    public void run() {
+
+        //cadena
+        String strCadena = "";
+
+        //agrega 30000 caracteres a una cadena vacía
+        for (int i = 0; i < 20000; ++i) {
+            //imprime el valor en la Salida
+            strCadena += "A";
+           this.yield();
+            //yield()sugiere al planficador Java que puede seleccionar otro hilo,
+           
+        }
+
+        System.out.println("Hilo de prioridad " + this.getPriority()
+                + " termina ahora");
+    }
+}
---
+++ UT3/UT3_4_PrioridadHilos/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author jhorn
+ *
+ */
+module UT3_4_PrioridadHilos {
+}
